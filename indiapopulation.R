eda2cba03bc1fc336d326183d8f2d83a0e621a57
Harish Nagpal <harnagpal@gmail.com>
2018-08-04 23:14:43 +0530

    Update indiapopulation.R

--- indiapopulation.R
+++ indiapopulation.R
@@ -1,4 +1,4 @@
-
+# set working directory
 setwd("D:\Harish\R practice projects\GitHub projects")
 getwd()
 
@@ -7,37 +7,39 @@ library(ggplot2)
 library(ggmap)
 library(ggalt)
 
-
+# ready the csv file
 mydata = read.csv("city pol.csv")
-
+# see the structure
 str(mydata)
-
+# convert city to character and population to numeric
 mydata$city <- as.character(mydata$city)
 mydata$population = as.numeric(mydata$pops)
 
+# have a look the data
 mydata
 str(mydata)
+# browse through the csv file and get lontitudes and latitudes of cities
 for (i in 1:nrow(mydata)) {
-#for (i in 1:10) {
   lat_lon = geocode(mydata[i,1])
   mydata$lon[i] = as.numeric(lat_lon[1])
   mydata$lat[i] = as.numeric(lat_lon[2])
   
 }
+# look at the data and analyze
 head(mydata, 10)
 
-
+# make dataframe with nececcesary data
 India_pops_data = data.frame(mydata$population, mydata$lon, mydata$lat, mydata$city)
-
+# make headings
 colnames(India_pops_data) = c('popultion','lon','lat', 'city')
-
+# look at the data and analyze
 head(India_pops_data, 10)
 
-
+# Get map for India 
 googleIndia = as.numeric(geocode("india"))
 IndiaMap = ggmap(get_googlemap(center=googleIndia, scale=2, zoom=5), extent="normal")
 
-
+# use GGplot for plotting
 
 IndiaMap +   
   geom_point(aes(x=lon, y=lat, col = city), data=India_pops_data, 
